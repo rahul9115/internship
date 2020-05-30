5ebaf731fa0355072022560a4fbe8d16b8755a84
rahul <rahul9115>
2020-05-30 23:32:32 +0530

    regression

--- getdata.py
+++ getdata.py
@@ -4,5 +4,5 @@ url = 'https://trak.in/india-startup-funding-investment-2015/'
 response = get(url)
 print(response.text[:500])
 html_soup = BeautifulSoup(response.text, 'html.parser')
-movie_containers = html_soup.find_all('div', class_='dataTables_wrapper')
+movie_containers = html_soup.find_all('table', class_='tablepress-46')
 print(movie_containers)
